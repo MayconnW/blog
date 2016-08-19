ee44299b42aec9319341bd3b5cdd1a9cc82807fe
mayconn <mayconnsuper1064@gmail.com>
2016-08-19 17:43:24 +0000

    Pause to try facebook login into other branch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     #@posts = Post.order(created_at: :desc).page(params[:page]).per(4)
     @posts = Post.order(:id).page(params[:page]).per(6)
+    #@posts = Post.new();
   end
   
 end
